db245601d9c74f493057585adfa199b7c17880cd
Евлахина Анна <35760192+EvaFlareon@users.noreply.github.com>
2018-04-18 12:50:32 +0300

    Update Python_Les12.py

--- Python_Les12.py
+++ Python_Les12.py
@@ -3,38 +3,37 @@ import os
 migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-run_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), migrations)
+run_dir = os.path.join(current_dir, migrations)
 
 
 def all_files():
     files_list = []
     for file in os.listdir(run_dir):
-        if (file.split('.')[-1]) == 'sql':
+        if os.path.splitext(file)[1] == '.sql':
             files_list.append(file)
     return files_list
 
 
 def search_files(files, search_term):
-    file_list = []
     for file in files:
-        for term in search_term:
-            if file.lower().find(term) == -1:
-                continue
-            else:
-                file_list.append(file)
-    if len(file_list) == 0:
-        return 'Такой поиск ничего не дал'
-    print(file_list)
-    print('Всего файлов:', len(file_list))
-    return True
+        if file.lower().find(search_term) == -1:
+            files.remove(file)
+        else:
+            continue
+    if len(files) == 0:
+        print('Такой поиск ничего не дал')
+    print(files)
+    print('Всего файлов:', len(files))
 
 
-while True:
+def search():
     files = all_files()
-    search_term = []
-    user_search = input('Введите строку (для выхода наберите exit): ').lower()
-    if user_search == 'exit':
-        break
-    else:
-        search_term.append(user_search)
-        search_files(files, search_term)
+    while True:
+        user_search = input('Введите строку (для выхода наберите exit): ').lower()
+        if user_search == 'exit':
+            break
+        else:
+            search_files(files, user_search)
+
+
+search()
